581e9e0e2df7c7888b6fb7e804e1746581a3d9be
Olamide Okunola <olamideokunola@yahoo.com>
2020-05-19 19:17:31 +0100

    Media & Static route changes

--- web/bizops_backend/bizops_backend/settings.py
+++ web/bizops_backend/bizops_backend/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
 
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__),'/uploaded/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/uploaded/')
