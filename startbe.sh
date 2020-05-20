37f066527cb3450d3d99555cf5dd65c87058961c
Olamide Okunola <olamideokunola@yahoo.com>
2020-05-20 17:15:04 +0100

    Update to server startup script

--- startbe.sh
+++ startbe.sh
@@ -2,3 +2,6 @@
 # My first script
 
 echo "Hello World!"
+echo `source /bizopsbe/bizops_be/env/bin/activate`
+echo `cd /bizopsbe/bizops_be/web/bizops_backend/`
+echo `python manage.py runserver 0.0.0.0:8000`
